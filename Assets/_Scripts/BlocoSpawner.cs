671f95a4870de0988ad8067912b9799697fda824
emanuellemoco <emanuellesmoco@gmail.com>
2021-03-02 22:23:11 -0300

    sprite bloco roxo

--- Assets/_Scripts/BlocoSpawner.cs
+++ Assets/_Scripts/BlocoSpawner.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BlocoSpawner : MonoBehaviour
 {
   public GameObject Bloco;
-  //blocoAzul, etc...
   GameManager gm;
   Bloco bloco; //?
 
@@ -23,10 +22,10 @@ public class BlocoSpawner : MonoBehaviour
           foreach (Transform child in transform) {
               GameObject.Destroy(child.gameObject);
           }
-          for(int i = 0; i < 11; i++)
+          for(int i = 0; i < 10; i++)
           {
               for(int j = 0; j < 4; j++){
-                  Vector3 posicao = new Vector3(-7.9f + 1.58f * i, 3.8f - 0.55f * j);
+                  Vector3 posicao = new Vector3(-8f + 2f * i, 4f - 1f * j);
 
                   Instantiate(Bloco, posicao, Quaternion.identity, transform);
   
@@ -39,7 +38,7 @@ public class BlocoSpawner : MonoBehaviour
   {
       //VOLTAR PARA 0
       //Quando os blocos acabam, o jogo entra no estado ENDGAME
-      if (transform.childCount <= 39 && gm.gameState == GameManager.GameState.GAME)
+      if (transform.childCount <= 0 && gm.gameState == GameManager.GameState.GAME)
       {
           gm.ChangeState(GameManager.GameState.ENDGAME);
           //RESETAR A POSICAO DA BOLA!!
